d49e52877d7c5ad314d382e15049029a24c8fb18
Oli <krasolha@gmail.com>
2022-01-30 18:11:32 -0500

    The basic structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 SolarSystem milkyWay = new SolarSystem();
+milkyWay.sun=1;
+milkyWay.Moon=2;
+milkyWay.planets=3;
 
     }
 }
